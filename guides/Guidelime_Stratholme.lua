7eb38c17eac3a5a1b7a961ac1a4e62be5d251c19
Cody York <coyofroyo@gmail.com>
2026-06-30 21:48:24 -0400

    Add custom Guidelime leveling guides for Uldaman, Zul'Farrak, Sunken Temple, Maraudon, Stratholme, and Blackrock Spire

---
+++ guides/Guidelime_Stratholme.lua
@@ -0,0 +1,55 @@
+Guidelime.registerGuide([[
+[GA Alliance][GA Horde]
+[N55-60 Stratholme Undead Quests]
+[D Comprehensive guide for Stratholme Undead quests and 45-minute Baron Run]
+
+-- Prerequisites & Quest Gathering
+Step: Gather quests at Light's Hope Chapel, Eastern Plaguelands:
+- Accept [QA5212 The Flesh Does Not Lie] from Betina Bigglezink: Go to [G75.0,52.0 Eastern Plaguelands]
+- Accept [QA5213 The Active Agent] from Betina Bigglezink: Go to [G75.0,52.0 Eastern Plaguelands] (Requires completing The Flesh Does Not Lie)
+- Accept [QA5263 Above and Beyond] from Duke Nicholas Zverenhoff: Go to [G75.0,52.0 Eastern Plaguelands]
+- Accept [QA5243 Houses of the Holy] from Leonid Barthalomew the Revered: Go to [G75.0,52.0 Eastern Plaguelands]
+- [D2 Step] Accept [QA8963 Dead Man's Plea] from Anthion Harmon (Ghost outside Service Gate): Go to [G27.0,12.0 Eastern Plaguelands]
+
+-- Entering the Dungeon
+Go to Service Gate (Undead Entrance): Go to [G27,12 Eastern Plaguelands]
+- Requires the Key to the City (from Magistrate Barthilas) or someone to open it.
+- Note: Aurius stands at the entrance and accepts [QT5123 The Medallion of Faith].
+Enter the instance portal.
+
+-- Inside the Instance
+Step: Clear the 3 Ziggurats:
+- Loot Stratholme Holy Water from supply crates throughout the dungeon for [QC5243].
+- Collect Plagued Flesh Samples from ghouls/undead for [QC5212] / [QC5213].
+- Go to Nerub'enkan's Ziggurat:
+  - Defeat Nerub'enkan: Target [TAR10438 Nerub'enkan]
+  - Enter the ziggurat and kill all Acolytes inside.
+- Go to Baroness Anastari's Ziggurat:
+  - Defeat Baroness Anastari: Target [TAR10436 Baroness Anastari]
+  - Enter the ziggurat and kill all Acolytes inside.
+- Go to Maleki the Pallid's Ziggurat:
+  - Defeat Maleki the Pallid: Target [TAR10437 Maleki the Pallid]
+  - Enter the ziggurat and kill all Acolytes inside.
+  - Complete [QC5213] by collecting Scourge Data from the ziggurat.
+
+Step: Slaughter Square & Baron Run:
+- Defeat Magistrate Barthilas: Target [TAR10435 Magistrate Barthilas]
+  - Note: Drops Key to the City.
+- Proceed through the gate to Slaughter Square.
+- Defeat the Abominations in the courtyard to spawn Ramstein.
+- Defeat Ramstein the Gorger: Target [TAR10439 Ramstein the Gorger]
+- Kill the waves of Black Guard Sentries.
+- Enter Baron's lair.
+- [D2 Step] **Timed Run:** Defeat Baron Rivendare within 45 minutes of entering the dungeon to save Ysida Harmon for [QC8963].
+- Defeat Baron Rivendare: Target [TAR10440 Baron Rivendare]
+- Loot Baron Rivendare's Head for [QC5263].
+
+-- Hearth & Turn-ins
+Use your [H] Hearthstone.
+Turn in quests at Light's Hope Chapel:
+- Turn in [QT5212 The Flesh Does Not Lie]
+- Turn in [QT5213 The Active Agent]
+- Turn in [QT5263 Above and Beyond]
+- Turn in [QT5243 Houses of the Holy]
+[D2 Step] Turn in [QT8963 Dead Man's Plea] to Anthion Harmon outside the dungeon.
+]], "OurAzeroth Guides")
---
+++ guides/Guidelime_ZulFarrak.lua
@@ -0,0 +1,66 @@
+Guidelime.registerGuide([[
+[GA Alliance][GA Horde]
+[N44-54 Zul'Farrak Quests]
+[D Comprehensive guide for all Zul'Farrak dungeon quests and boss order]
+
+-- Prerequisites & Quest Gathering
+Step: Gather quests in Gadgetzan, Tanaris:
+- Accept [QA2865 Scarab Shells] from Tran'rek: Go to [G51.4,28.8 Tanaris]
+- Accept [QA2769 Troll Temper] from Trenton Lighthammer: Go to [G51.6,28.8 Tanaris]
+- Accept [QA2843 The Divino-matic Rod] from Chief Engineer Bilgewhizzle: Go to [G52.4,28.6 Tanaris]
+
+Step: Gather quests in Steamwheedle Port, Tanaris:
+- Accept [QA2605 The Prophecy of Mosh'aru] from Yeh'kinya: Go to [G67.0,22.0 Tanaris]
+
+Step: Gather quests in Shimmering Flats, Thousand Needles:
+- Accept [QA2862 Tiara of the Deep] from Tally Zapmaster: Go to [G78.4,77.2 Thousand Needles]
+- Accept [QA2770 Gahz'rilla] from Wizzle Brassbolts: Go to [G78.2,77.4 Thousand Needles]
+
+Step: Mallet of Zul'Farrak Preparation (Optional):
+- Travel to Altar of Zul in Hinterlands to get the Sacred Mallet from Qiaga the Keeper.
+- Take it to Jintha'alor to forge the Mallet of Zul'Farrak.
+
+-- Entering the Dungeon
+Go to Zul'Farrak Entrance: Go to [G39,21 Tanaris]
+Enter the instance portal.
+
+-- Inside the Instance
+Defeat Antu'sul: Target [TAR7267 Antu'sul]
+- Complete [QC2862] (Tiara of the Deep).
+Defeat Theka the Martyr: Target [TAR7272 Theka the Martyr]
+- Loot the First Mosh'aru Tablet for [QC2605].
+Collect 5 Scarab Shells: Collect [CI9311,5 Unbroken Scarab Shells] for [QC2865].
+Collect 20 Troll Tempers: Collect [CI9292,20 Troll Temper] for [QC2769].
+Defeat Witch Doctor Zum'rah: Target [TAR7271 Witch Doctor Zum'rah]
+
+The Stairs Event:
+- Go to the top of the stairs and speak to the Executioner to start the event.
+- Defend the NPCs during the waves of trolls.
+- Defeat Nekrum Gutchewer: Target [TAR7275 Nekrum Gutchewer]
+- Defeat Shadowpriest Sezz'ziz: Target [TAR7274 Shadowpriest Sezz'ziz]
+- Speak to Sergeant Bly and select the dialogue option to fight him.
+- Defeat Sergeant Bly and loot the Divino-matic Rod for [QC2843].
+
+Defeat Hydromancer Velratha: Target [TAR7269 Hydromancer Velratha]
+- Loot the Second Mosh'aru Tablet for [QC2605].
+Defeat Chief Ukorz Sandscalp (End Boss): Target [TAR7273 Chief Ukorz Sandscalp]
+
+Summon Gahz'rilla:
+- Clear the pool area.
+- Use the Mallet of Zul'Farrak on the gong to summon the boss.
+- Defeat Gahz'rilla: Target [TAR7270 Gahz'rilla] for [QC2770].
+
+-- Hearth & Turn-ins
+Use your [H] Hearthstone.
+Turn in quests in Gadgetzan, Tanaris:
+- Turn in [QT2865 Scarab Shells]: Go to [G51.4,28.8 Tanaris]
+- Turn in [QT2769 Troll Temper]: Go to [G51.6,28.8 Tanaris]
+- Turn in [QT2843 The Divino-matic Rod]: Go to [G52.4,28.6 Tanaris]
+
+Turn in quests in Steamwheedle Port, Tanaris:
+- Turn in [QT2605 The Prophecy of Mosh'aru]: Go to [G67.0,22.0 Tanaris]
+
+Turn in quests in Shimmering Flats, Thousand Needles:
+- Turn in [QT2862 Tiara of the Deep]: Go to [G78.4,77.2 Thousand Needles]
+- Turn in [QT2770 Gahz'rilla]: Go to [G78.2,77.4 Thousand Needles]
+]], "OurAzeroth Guides")
